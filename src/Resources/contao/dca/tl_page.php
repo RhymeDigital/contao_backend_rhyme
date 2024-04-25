ff09734f8eb934b045b00c4e743712a5d39b73f2
rhymadam <adam@rhymedigital.com>
2024-04-25 16:15:03 -0400

    Working on Contao 5.3 compat

--- src/Resources/contao/dca/tl_page.php
+++ src/Resources/contao/dca/tl_page.php
@@ -16,34 +16,34 @@ namespace {
     use Contao\CoreBundle\ContaoCoreBundle;
     use Contao\CoreBundle\DataContainer\PaletteManipulator;
 
-    $dca = &$GLOBALS['TL_DCA']['tl_page'];
-
-    /**
-     * Operations
-     */
-    ArrayUtil::arrayInsert($dca['list']['operations'], 1,
-        ['editheader' =>  [
-            'href' => 'act=edit',
-            'icon' => 'header.gif'
-        ]]
-    );
-
-    $dca['list']['operations']['edit'] = [
-        'href'                => 'table=tl_article',
-        'icon'                => 'edit.gif'
-    ];
-
-    unset($dca['list']['operations']['articles']);
-
-    /**
-     * Fields
-     */
-    $dca['fields']['layout']['wizard'] = $dca['fields']['layout']['wizard'] ?? [
-        [Callbacks::class, 'editLayout']
-    ];
-    $dca['fields']['subpageLayout']['wizard'] = $dca['fields']['subpageLayout']['wizard'] ?? [
-        [Callbacks::class, 'editLayout']
-    ];
+//    $dca = &$GLOBALS['TL_DCA']['tl_page'];
+//
+//    /**
+//     * Operations
+//     */
+//    ArrayUtil::arrayInsert($dca['list']['operations'], 1,
+//        ['editheader' =>  [
+//            'href' => 'act=edit',
+//            'icon' => 'header.gif'
+//        ]]
+//    );
+//
+//    $dca['list']['operations']['edit'] = [
+//        'href'                => 'table=tl_article',
+//        'icon'                => 'edit.gif'
+//    ];
+//
+//    unset($dca['list']['operations']['articles']);
+//
+//    /**
+//     * Fields
+//     */
+//    $dca['fields']['layout']['wizard'] = $dca['fields']['layout']['wizard'] ?? [
+//        [Callbacks::class, 'editLayout']
+//    ];
+//    $dca['fields']['subpageLayout']['wizard'] = $dca['fields']['subpageLayout']['wizard'] ?? [
+//        [Callbacks::class, 'editLayout']
+//    ];
 
 
 }
